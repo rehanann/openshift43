77d83d4bdf7929a508e560161df21b3f14400fc4
Rehan Anwar <rehanann@gmail.com>
2020-04-24 23:20:56 +0100

    base infra setup

--- terraform/scripts/init.sh
+++ terraform/scripts/init.sh
@@ -2,10 +2,13 @@
 
 # Install mandatory packages before setting up system.
 
+dnf makecache
+dnf -y install epel-release
 declare -a Package=("ansible" "wget" "git" "unzip" "zip" "curl")
-for val in $Package[@]; do 
-    yum -y install $val
+for val in "${Package[@]}"; do 
+    dnf -y install $val
 done
 
+dnf -y update
 
 ansible-playbook --connection=local 127.0.0.1 main.yaml
